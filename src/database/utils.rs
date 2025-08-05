973f9f75baedc07f5c37bf5f5c075cb970081d3e
Kyle Dougan <5292268+f0ursqu4r3@users.noreply.github.com>
2025-08-05 11:04:58 -1000

    refactor: Improve whitespace handling in SQL query function for consistency

--- src/database/utils.rs
+++ src/database/utils.rs
@@ -1,14 +1,21 @@
 use regex::Regex;
 
 pub fn sql(query: &str) -> String {
-    let cleaned = query.split_whitespace().collect::<Vec<&str>>().join(" ");
+    let cleaned = query
+        .trim()
+        .split_whitespace()
+        .collect::<Vec<&str>>()
+        .join(" ");
+
     let re = Regex::new(r"\?").unwrap();
     let mut param_index = 1;
     let mut result = cleaned;
+
     while let Some(mat) = re.find(&result) {
         let replacement = format!("${}", param_index);
         result.replace_range(mat.range(), &replacement);
         param_index += 1;
     }
-    result
+
+    result.trim().to_string()
 }
